50191a14ae809b06846c27d9011e04995ff3244b
Sefa Ümit Oray <s.umitoray@gmail.com>
2016-07-29 22:01:36 +0300

    Initial project creation & added connectionStrings.config.example file

---
+++ Eveo7/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Eveo7.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Eveo7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eveo7.WebApiApplication" Language="C#" %>
